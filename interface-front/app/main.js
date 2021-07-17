f78aef63b8b7ceb7c58ba2b0bf230b2034059acb
caparicio-esd <caparicio@esdmadrid.es>
2021-07-17 12:17:59 +0200

    set up front

--- interface-front/app/main.js
+++ interface-front/app/main.js
@@ -1,12 +1,14 @@
-import "../assets/sass/style.sass";
+import "../assets/sass/main.sass";
 import { io } from "socket.io-client";
- 
+import angular from "angular"
+import { Chart } from "chart.js";
+import interact from "interactjs"
+
 
 const socket = io("http://localhost:1027", {});
 socket.on("connect", () => {
   console.log(socket.id);
-  socket.on("data", data => {
+  socket.on("data", (data) => {
     console.log(data);
-  })
-})
-
+  });
+});
